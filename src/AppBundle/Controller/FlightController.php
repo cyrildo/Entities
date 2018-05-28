b85330c2f17f20598d8d90936cdf855321470ad4
Cyril Do <cyrildo@yahoo.fr>
2018-05-28 17:15:15 +0200

    Quête Services

--- src/AppBundle/Controller/FlightController.php
+++ src/AppBundle/Controller/FlightController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Flight;
+use AppBundle\Service\FlightInfo;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
@@ -63,11 +64,23 @@ class FlightController extends Controller
      * @Route("/{id}", name="flight_show")
      * @Method("GET")
      */
-    public function showAction(Flight $flight)
+    public function showAction(Flight $flight, FlightInfo $flightInfo)
     {
         $deleteForm = $this->createDeleteForm($flight);
+        $distance = $flightInfo->getDistance(
+            $flight->getDeparture()->getLatitude(),
+            $flight->getDeparture()->getLongitude(),
+            $flight->getArrival()->getLatitude(),
+            $flight->getArrival()->getLongitude()
+        );
+        $duration = $flightInfo->getTime(
+            $distance,
+            $flight->getPlane()->getCruiseSpeed()
+        );
 
         return $this->render('flight/show.html.twig', array(
+            'duration'=>$duration,
+            'distance'=>$distance,
             'flight' => $flight,
             'delete_form' => $deleteForm->createView(),
         ));
